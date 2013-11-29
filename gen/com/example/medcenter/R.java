2fbdd4b996ca807ce81814763ca7ea6acff195ea
Jason Paolasini <paolasja@mcmaster.ca>
2013-11-28 23:43:45 -0500

    GraphingComplete
    The entire graphing function has been implemented. All changes to R
    file and Manifest are included

--- gen/com/example/medcenter/R.java
+++ gen/com/example/medcenter/R.java
@@ -17,39 +17,47 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int domain_label_font_size=0x7f050004;
+        public static final int domain_tick_label_font_size=0x7f050007;
+        public static final int legend_text_font_size=0x7f050008;
+        public static final int pie_segment_label_font_size=0x7f050002;
+        public static final int range_label_font_size=0x7f050005;
+        public static final int range_tick_label_font_size=0x7f050006;
+        public static final int title_font_size=0x7f050003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080019;
-        public static final int btnIntend=0x7f080015;
-        public static final int btnIntendS=0x7f080016;
-        public static final int button1=0x7f08000c;
-        public static final int button2=0x7f08000d;
-        public static final int button3=0x7f080012;
-        public static final int button4=0x7f080013;
-        public static final int button5=0x7f080018;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080003;
-        public static final int editText3=0x7f080005;
-        public static final int editText4=0x7f080007;
-        public static final int editText5=0x7f080009;
-        public static final int editText6=0x7f08000b;
-        public static final int errorMessage=0x7f08000e;
-        public static final int image=0x7f080014;
-        public static final int imageView1=0x7f080017;
-        public static final int password=0x7f080011;
-        public static final int patientName=0x7f08000f;
-        public static final int textView1=0x7f08000a;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080006;
-        public static final int textView5=0x7f080008;
-        public static final int textView6=0x7f080000;
-        public static final int userName=0x7f080010;
+        public static final int action_settings=0x7f09001a;
+        public static final int btnIntend=0x7f090016;
+        public static final int btnIntendS=0x7f090017;
+        public static final int button1=0x7f09000c;
+        public static final int button2=0x7f09000d;
+        public static final int button3=0x7f090013;
+        public static final int button4=0x7f090014;
+        public static final int button5=0x7f090019;
+        public static final int editText1=0x7f090001;
+        public static final int editText2=0x7f090003;
+        public static final int editText3=0x7f090005;
+        public static final int editText4=0x7f090007;
+        public static final int editText5=0x7f090009;
+        public static final int editText6=0x7f09000b;
+        public static final int errorMessage=0x7f09000f;
+        public static final int image=0x7f090015;
+        public static final int imageView1=0x7f090018;
+        public static final int mySimpleXYPlot=0x7f09000e;
+        public static final int password=0x7f090012;
+        public static final int patientName=0x7f090010;
+        public static final int textView1=0x7f09000a;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090006;
+        public static final int textView5=0x7f090008;
+        public static final int textView6=0x7f090000;
+        public static final int userName=0x7f090011;
     }
     public static final class layout {
         public static final int activity_adddoctor=0x7f030000;
@@ -64,20 +72,20 @@ public final class R {
         public static final int activity_userinfo=0x7f030009;
     }
     public static final class menu {
-        public static final int graphing=0x7f070000;
-        public static final int login=0x7f070001;
+        public static final int graphing=0x7f080000;
+        public static final int login=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int album_name=0x7f050008;
-        public static final int app_name=0x7f050000;
-        public static final int btnIntend=0x7f050004;
-        public static final int btnIntendS=0x7f050005;
-        public static final int btnIntendV=0x7f050006;
-        public static final int cannot=0x7f050007;
-        public static final int hello=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_graphing=0x7f050009;
+        public static final int action_settings=0x7f060001;
+        public static final int album_name=0x7f060008;
+        public static final int app_name=0x7f060000;
+        public static final int btnIntend=0x7f060004;
+        public static final int btnIntendS=0x7f060005;
+        public static final int btnIntendV=0x7f060006;
+        public static final int cannot=0x7f060007;
+        public static final int hello=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_graphing=0x7f060009;
     }
     public static final class style {
         /** 
@@ -101,10 +109,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int line_point_formatter_with_plf1=0x7f040000;
+        public static final int line_point_formatter_with_plf2=0x7f040001;
     }
 }
--- src/com/example/medcenter/GraphingActivity.java
+++ src/com/example/medcenter/GraphingActivity.java
@@ -1,24 +1,65 @@
 package com.example.medcenter;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+import java.util.Arrays;
 
-public class GraphingActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_graphing);
-		Intent intent = getIntent();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.graphing, menu);
-		return true;
-	}
-
+ 
+/**
+ * A straightforward example of using AndroidPlot to plot some data.
+ */
+public class GraphingActivity extends Activity
+{
+ 
+    private XYPlot plot;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+ 
+        // fun little snippet that prevents users from taking screenshots
+        // on ICS+ devices :-)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                                 WindowManager.LayoutParams.FLAG_SECURE);
+ 
+        setContentView(R.layout.activity_graphing);
+ 
+        // initialize our XYPlot reference:
+        plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+ 
+        // Create a couple arrays of y-values to plot:
+       Integer[] xVals = {1,2,3,4,5,6};
+       Integer[] yVals ={120,130,124,125,127,160};
+        
+ 
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(xVals),          // SimpleXYSeries takes a List so turn our array into a List
+                Arrays.asList(yVals), 
+                "Weight");                             // Set the display title of the series
+ 
+       
+ 
+        // Create a formatter to use for drawing a series using LineAndPointRenderer
+        // and configure it from xml:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(getApplicationContext(),
+                R.xml.line_point_formatter_with_plf1);
+ 
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+ 
+    
+ 
+        // reduce the number of range labels
+        plot.setTicksPerRangeLabel(3);
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
+ 
+    }
 }
