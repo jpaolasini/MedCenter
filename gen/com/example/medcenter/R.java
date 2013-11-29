0616294ad546e0f805cbbb923545e12c0ebb5035
Jason Paolasini <paolasja@mcmaster.ca>
2013-11-28 23:54:56 -0500

    R fixed

--- gen/com/example/medcenter/R.java
+++ gen/com/example/medcenter/R.java
@@ -31,20 +31,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09001a;
+        public static final int action_settings=0x7f090020;
         public static final int btnIntend=0x7f090016;
         public static final int btnIntendS=0x7f090017;
         public static final int button1=0x7f09000c;
         public static final int button2=0x7f09000d;
         public static final int button3=0x7f090013;
         public static final int button4=0x7f090014;
-        public static final int button5=0x7f090019;
+        public static final int button5=0x7f09001f;
         public static final int editText1=0x7f090001;
         public static final int editText2=0x7f090003;
         public static final int editText3=0x7f090005;
         public static final int editText4=0x7f090007;
         public static final int editText5=0x7f090009;
         public static final int editText6=0x7f09000b;
+        public static final int editText7=0x7f09001b;
+        public static final int editText8=0x7f09001d;
         public static final int errorMessage=0x7f09000f;
         public static final int image=0x7f090015;
         public static final int imageView1=0x7f090018;
@@ -52,11 +54,15 @@ public final class R {
         public static final int password=0x7f090012;
         public static final int patientName=0x7f090010;
         public static final int textView1=0x7f09000a;
+        public static final int textView10=0x7f09001e;
         public static final int textView2=0x7f090002;
         public static final int textView3=0x7f090004;
         public static final int textView4=0x7f090006;
         public static final int textView5=0x7f090008;
         public static final int textView6=0x7f090000;
+        public static final int textView7=0x7f090019;
+        public static final int textView8=0x7f09001a;
+        public static final int textView9=0x7f09001c;
         public static final int userName=0x7f090011;
     }
     public static final class layout {
