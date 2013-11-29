42663d107f4014ecdcb31ce0929b15de8df74e58
Fates4Productions <fates4productions@gmail.com>
2013-11-28 21:59:27 -0500

    god dammit pedometer

--- src/com/example/medcenter/PedometerActivity.java
+++ src/com/example/medcenter/PedometerActivity.java
@@ -14,50 +14,12 @@ import android.hardware.SensorManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PedometerActivity extends Activity implements SensorEventListener {
-  private float mLastX, mLastY, mLastZ;
-  private boolean mInitialized;
-  private SensorManager mSensorManager;
-  private Sensor mAccelerometer;
-  private final float NOISE = (float) 2.0;
+public class PedometerActivity extends Activity{
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_pedometer);
     Intent intent = getIntent();
-    mInitialized = false;
-    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    mSensorManager.registerListener(this, mAccelerometer,
-        SensorManager.SENSOR_DELAY_NORMAL);
-  }
-
-  protected void onResume() {
-
-    super.onResume();
-
-    mSensorManager.registerListener(this, mAccelerometer,
-        SensorManager.SENSOR_DELAY_NORMAL);
-
-  }
-
-  protected void onPause() {
-
-    super.onPause();
-
-    mSensorManager.unregisterListener(this);
-
-  }
-
-  @Override
-  public void onAccuracyChanged(Sensor arg0, int arg1) {
-    // TODO Auto-generated method stub
-  }
-
-  @Override
-  public void onSensorChanged(SensorEvent arg0) {
-    EditText editText = (EditText) findViewById(R.id.textView1);
-    editText.setText("wat");
   }
 
 }
