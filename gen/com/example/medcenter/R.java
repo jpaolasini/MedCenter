32bdbd29f078b1fc94960dca359d9a25cf0e2faf
Jason Paolasini <paolasja@mcmaster.ca>
2013-12-02 21:18:43 -0500

    CalendarChanges

--- gen/com/example/medcenter/R.java
+++ gen/com/example/medcenter/R.java
@@ -36,8 +36,11 @@ public final class R {
         public static final int title_font_size=0x7f050003;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int medcenter=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int calendar_week=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int medcenter=0x7f020003;
+        public static final int typeb_calendar_today=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0033;
--- src/com/example/medcenter/ScheduleActivity.java
+++ src/com/example/medcenter/ScheduleActivity.java
@@ -1,17 +1,30 @@
 package com.example.medcenter;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class ScheduleActivity extends Activity {
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_schedule);
-    Intent intent = getIntent();
+    
     // String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
     // EditText editText = (EditText) findViewById(R.id.textView1);
   }
+  public void calenderevent(Calendar begintime, Calendar endtime){
+
+	    Intent intent = new Intent(Intent.ACTION_EDIT);
+	    intent.setType("vnd.android.cursor.item/event");
+	    intent.putExtra("beginTime", begintime.getTimeInMillis());
+	    intent.putExtra("allDay", true);
+	    intent.putExtra("rrule", "FREQ=YEARLY");
+	    intent.putExtra("endTime", endtime.getTimeInMillis()+60*60*1000);
+	    intent.putExtra("title", "A Test Event from android app");
+	    startActivity(intent);
+	}
 }
