47cc35b0e5b8e9ed78da03558635d4fb07748b9a
Jason Paolasini <paolasja@mcmaster.ca>
2013-12-02 22:54:56 -0500

    COMMIT CHANGES

--- gen/com/example/medcenter/R.java
+++ gen/com/example/medcenter/R.java
@@ -109,7 +109,6 @@ public final class R {
         public static final int activity_register=0x7f030009;
         public static final int activity_schedule=0x7f03000a;
         public static final int activity_userinf=0x7f03000b;
-        public static final int activity_userinfo=0x7f03000c;
     }
     public static final class menu {
         public static final int graphing=0x7f090000;
--- src/com/example/medcenter/GraphingActivity.java
+++ src/com/example/medcenter/GraphingActivity.java
@@ -9,12 +9,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.WindowManager;
-<<<<<<< HEAD
-=======
 
 import android.widget.TextView;
 
->>>>>>> 116788722c04bb1a7f78378a857e44dfa2bf25c2
 
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
@@ -28,10 +25,7 @@ import com.androidplot.xy.XYSeries;
 public class GraphingActivity extends Activity {
 
   private XYPlot plot;
-<<<<<<< HEAD
-=======
 
->>>>>>> 4c30d8fb6e487c00c334edfe4b592481c01cbcc7
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -41,23 +35,14 @@ public class GraphingActivity extends Activity {
         Environment.DIRECTORY_DOWNLOADS).toString()
         + "/";
     
-<<<<<<< HEAD
-    String fileName = UserInformation.userName + ".txt";
-    Log.d("FILENAME", fileName);
-=======
 
     String fileName = UserInformation.userName+ ".txt";
-    TextView t = new TextView(this);
-    t = (TextView) findViewById(R.id.test);
-    t.setText(UserInformation.userName);
->>>>>>> 116788722c04bb1a7f78378a857e44dfa2bf25c2
     
     //Read existing user data from file.
     String data;
 	try {
 		data = FileHandler.ReadFile(directoryPath + "medCenter/", fileName);
 		String[] patientInfo = UserInformation.parseInfo(data);
-		Log.d("PATIENTINFO", patientInfo[6]);
 
 	    // fun little snippet that prevents users from taking screenshots
 	    // on ICS+ devices :-)
@@ -80,26 +65,12 @@ ageString = ageString.replace("[","");
 weightString = weightString.replace("[","");
 
 String[] weight = weightString.split(":");
-String[] age = ageString.split(":");
+
+String[] age = weightString.split(":");
 
 Integer[] ageArray = new Integer[age.length];
 Integer[] weightArray = new Integer[weight.length];
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-for(int i = 0; i < ageArray.length; i++){
-	ageArray[i] = Integer.parseInt(age[i]);
-}
-for(int i = 0; i<weightArray.length; i++)
-{
-	weightArray[i] = Integer.parseInt(weight[i]);
-}
-=======
-weightArray[age.length] = 10;
->>>>>>> 116788722c04bb1a7f78378a857e44dfa2bf25c2
-=======
-
->>>>>>> 4c30d8fb6e487c00c334edfe4b592481c01cbcc7
 
 	    // initialize our XYPlot reference:
 	    plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
@@ -108,12 +79,8 @@ weightArray[age.length] = 10;
 	    Integer[] xVals = ageArray;
 	    Integer[] yVals = weightArray;
 	    
-	    Log.i("test",yVals.toString());
-	    
 	   
 
-	    Log.d("xVals", String.valueOf(xVals));
-	    Log.d("yVals", String.valueOf(yVals));
 	    // Turn the above arrays into XYSeries':
 	    XYSeries series1 = new SimpleXYSeries(Arrays.asList(xVals), // SimpleXYSeries
 	                                                                // takes a List
@@ -127,7 +94,7 @@ weightArray[age.length] = 10;
 	    LineAndPointFormatter series1Format = new LineAndPointFormatter();
 	    series1Format.setPointLabelFormatter(new PointLabelFormatter());
 	    series1Format.configure(getApplicationContext(),
-	        R.xml.line_point_formatter_with_plf1);
+	    R.xml.line_point_formatter_with_plf1);
 
 	    // add a new series' to the xyplot:
 	    plot.addSeries(series1, series1Format);
@@ -135,6 +102,8 @@ weightArray[age.length] = 10;
 	    // reduce the number of range labels
 	    plot.setTicksPerRangeLabel(3);
 	    plot.getGraphWidget().setDomainLabelOrientation(-45);
+	    
+	    
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
